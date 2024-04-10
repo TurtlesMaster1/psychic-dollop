4eb9b2f7e36d531fa06d79f4a90e1aca42816836
danyoonyoutube <166571667+danyoonyoutube@users.noreply.github.com>
2024-04-09 18:27:26 -0700

    lol
    idk

--- py.py
+++ py.py
@@ -4,4 +4,5 @@ storage.file.addd(3,4)
 name = input("What is your name? ")
 
 # Greet the user
-print(f"Hello, {name}! Welcome to the Python script.")
+print(f"Hello, {name}!")
+print(f"U suck bolls {name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}")
