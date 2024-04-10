3ec3f1b8620690d710d26ab75f12b47093dbdf00
TurtlesMaster1 <nathan.xing001@gmail.com>
2024-04-10 07:17:50 -0700

    Update py.py

--- py.py
+++ py.py
@@ -1,8 +1,3 @@
 import storage.file
 storage.file.addd(3,4)
 # Prompt the user for their name
-name = input("What is your name? ")
-
-# Greet the user
-print(f"Hello, {name}!")
-print(f"U suck bolls {name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}{name}")
